6a33b3d84688ec9e9f99272280e6d8c702e60121
Hiroki Noda <kubo39@gmail.com>
2020-01-21 13:58:08 +0900

    Windows support, not tested yet

--- source/ssh2/package.d
+++ source/ssh2/package.d
@@ -9,15 +9,16 @@ shared static this()
         import ssh2.ffi;
         assert(libssh2_init(LIBSSH2_INIT_NO_CRYPTO) == 0);
     }
+    else version (Windows)
+    {
+        import ssh2.ffi;
+        assert(libssh2_init(0) == 0);
+    }
     else static assert(false, "Unsupported platform.");
 }
 
 shared static ~this()
 {
-    version (Posix)
-    {
-        import ssh2.ffi;
-        libssh2_exit();
-    }
-    else static assert(false, "Unsupported platform.");
+    import ssh2.ffi;
+    libssh2_exit();
 }
